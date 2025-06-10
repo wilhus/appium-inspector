01c89e41161a3a29f1ba97216809b827e229c359
William Husar <william.husar@hotmail.com>
2025-06-10 10:47:14 +0200

    Add app layout

--- app/common/renderer/Root.jsx
+++ app/common/renderer/Root.jsx
@@ -1,4 +1,4 @@
-import {ConfigProvider, theme} from 'antd';
+import {App, ConfigProvider, Layout, theme} from 'antd';
 import {Suspense} from 'react';
 import {Provider, useSelector} from 'react-redux';
 import {MemoryRouter, Route, Routes} from 'react-router';
@@ -8,6 +8,7 @@ import InspectorPage from './containers/InspectorPage';
 import SessionPage from './containers/SessionPage';
 import i18n from './i18next';
 import {ipcRenderer} from './polyfills';
+import RootStyles from './Root.module.css';
 
 ipcRenderer.on('appium-language-changed', (event, message) => {
   if (i18n.language !== message.language) {
@@ -19,8 +20,6 @@ const getTheme = (isDark) => ({
   algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
   token: {
     fontSize: 12,
-    fontFamily:
-      "'Helvetica Neue For Number', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
   },
   components: {
     Tabs: {
@@ -34,15 +33,19 @@ const AppContent = () => {
 
   return (
     <ConfigProvider theme={getTheme(isDarkMode)}>
-      <MemoryRouter initialEntries={['/']}>
-        <Suspense fallback={<Spinner />}>
-          <Routes>
-            <Route path="/" element={<SessionPage />} />
-            <Route path="/session" element={<SessionPage />} />
-            <Route path="/inspector" element={<InspectorPage />} />
-          </Routes>
-        </Suspense>
-      </MemoryRouter>
+      <App>
+        <Layout className={RootStyles.appLayout}>
+          <MemoryRouter initialEntries={['/']}>
+            <Suspense fallback={<Spinner />}>
+              <Routes>
+                <Route path="/" element={<SessionPage />} />
+                <Route path="/session" element={<SessionPage />} />
+                <Route path="/inspector" element={<InspectorPage />} />
+              </Routes>
+            </Suspense>
+          </MemoryRouter>
+        </Layout>
+      </App>
     </ConfigProvider>
   );
 };
