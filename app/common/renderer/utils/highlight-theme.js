a305febf9666c1b1778f6fbc42bd37670cc69f0b
William Husar <william.husar@hotmail.com>
2025-06-10 14:59:23 +0200

    Use ES6 imports

--- app/common/renderer/utils/highlight-theme.js
+++ app/common/renderer/utils/highlight-theme.js
@@ -1,12 +1,15 @@
+import githubDarkCss from 'highlight.js/styles/github-dark.css?url';
+import intellijLightCss from 'highlight.js/styles/intellij-light.css?url';
+
 export const loadHighlightTheme = (isDarkMode) => {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
   link.type = 'text/css';
 
   if (isDarkMode) {
-    link.href = require.resolve('highlight.js/styles/github-dark.css');
+    link.href = githubDarkCss;
   } else {
-    link.href = require.resolve('highlight.js/styles/intellij-light.css');
+    link.href = intellijLightCss;
   }
 
   document.head.appendChild(link);
